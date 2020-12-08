53deb80324eef6ce30176ed42d7d3453aff7dd3c
pcmt <pcmt@users.noreply.github.com>
2020-12-08 12:16:43 +0000

    Create slides.js

---
+++ js/slides.js
@@ -0,0 +1,28 @@
+/**
+ * superMicro CMS
+ * ==============
+ * COPYRIGHT Patrick Taylor https://patricktaylor.com/
+ */
+
+/* Last updated 04 Aug 2020 */
+
+/* https://www.w3schools.com/howto/howto_js_slideshow.asp */
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = slides.length;}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
