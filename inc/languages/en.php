d76407b04bafd1c522c1dc531391bb6b673b0e40
pcmt <pcmt@users.noreply.github.com>
2020-12-08 12:15:06 +0000

    Add files via upload

---
+++ inc/languages/en.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * superMicro CMS
+ * ==============
+ * COPYRIGHT Patrick Taylor https://patricktaylor.com/
+ */
+
+/* Last updated 20 Sept 2020 */
+
+/* -------------------------------------------------- */
+// File /inc/content.php
+
+define("TEXT00", "Page last modified:");
+
+/* -------------------------------------------------- */
+// File /inc/login-form.php
+
+define("TEXT14", "Enter Password");
+
+/* -------------------------------------------------- */
+// File /e.php
+
+define("TEXT17", "You didn't enter your name.");
+define("TEXT18", "The name you enter must be two words maximum.");
+define("TEXT19", "You didn't enter a valid email address.");
+define("TEXT20", "You didn't enter a comment.");
+define("TEXT21", "Not sent. Please leave the third box empty.");
+define("TEXT22", "Not sent. Please do not submit web addresses.");
+define("TEXT23", "Forbidden. You are not authorized.");
+define("TEXT24", "Message from");
+define("TEXT25", "From:");
+define("TEXT26", "Sent by");
+define("TEXT27", "Your comment has been submitted, thank you.");
+define("TEXT28", "Error. Your comment was not sent.");
+define("TEXT48", "Stopwords found.");
+define("TEXT29", "Try again.");
+define("TEXT30", "Contact");
+define("TEXT31", "Contact form");
+define("TEXT32", "Your name (two words maximum)");
+define("TEXT33", "Your email");
+define("TEXT34", "Submit");
+
+/* -------------------------------------------------- */
+// File /inc/menu.php
+
+define("TEXT47", "Edit");
+
+/* -------------------------------------------------- */
+// File /s.php
+
+define("TEXT16", "Search");
+define("TEXT35", "Search Results");
+define("TEXT36", "Search results for");
+define("TEXT37", "match");
+define("TEXT38", "matches");
+define("TEXT40", "Sorry. Nothing found for");
+define("TEXT41", "At least 3 characters.");
+define("TEXT42", "pages searched in");
+define("TEXT43", "seconds");
+
+/* -------------------------------------------------- */
+// File /inc/extra-content.php
+
+define("TEXT50", "Comments");
+define("TEXT51", "No comments yet.");
+define("TEXT52", "Leave a comment");
+define("TEXT53", "Comments are closed.");
+define("TEXT54", "No comments found.");
+
+?>
---
+++ inc/languages/es.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * superMicro CMS
+ * ==============
+ * COPYRIGHT Patrick Taylor https://patricktaylor.com/
+ */
+
+/* Last updated 21 Sept 2020 */
+
+/* -------------------------------------------------- */
+// File /inc/content.php
+
+define("TEXT00", "&#218;ltima modificaci&#243;n de la p&#225;gina:");
+
+/* -------------------------------------------------- */
+// File /inc/login-form.php
+
+define("TEXT14", "Introduzca Contrase&#241;a");
+
+/* -------------------------------------------------- */
+// File /e.php
+
+define("TEXT17", "No ha introducido su nombre.");
+define("TEXT18", "El nombre que introduzca tiene que tener un m&#225;ximo de dos palabras.");
+define("TEXT19", "No ha introducido una direcci&#243;n de correo v&#225;lida.");
+define("TEXT20", "No ha introducido un comentario.");
+define("TEXT21", "No enviado. Por favor, deje el tercer cuadro vac&#237;o.");
+define("TEXT22", "No enviado. Por favor, no presente direcciones de p&#225;ginas web.");
+define("TEXT23", "Prohibido. No est&#225; autorizado.");
+define("TEXT24", "Mensaje de");
+define("TEXT25", "De:");
+define("TEXT26", "Enviado por");
+define("TEXT27", "Su comentario ha sido presentado, gracias.");
+define("TEXT28", "Error. Su comentario no fue enviado.");
+define("TEXT48", "Stopwords found."); // Translate?
+define("TEXT29", "Int&#233;ntalo otra vez.");
+define("TEXT30", "Contacto");
+define("TEXT31", "Formulario de contacto");
+define("TEXT32", "Su nombre (m&#225;ximo de dos palabras)");
+define("TEXT33", "Su direcci&#243;n de correo electr&#243;nico");
+define("TEXT34", "Presentar");
+
+/* -------------------------------------------------- */
+// File /inc/menu.php
+
+define("TEXT47", "Edite");
+
+/* -------------------------------------------------- */
+// File /s.php
+
+define("TEXT16", "Buscar");
+define("TEXT35", "Resultados de busqueda");
+define("TEXT36", "Resultados de");
+define("TEXT37", "coincidente");
+define("TEXT38", "coincidentes");
+define("TEXT40", "Lo siento. No se ha podido encontrar nada sobre");
+define("TEXT41", "No se ha podido encontrar nada.");
+define("TEXT42", "p&#225;ginas buscadas en");
+define("TEXT43", "segundos");
+
+/* -------------------------------------------------- */
+// File /inc/extra-content.php
+
+define("TEXT50", "Comentarios");
+define("TEXT51", "Sin comentarios a&#250;n.");
+define("TEXT52", "Deja un comentario");
+define("TEXT53", "Los comentarios est&#225;n cerrados.");
+define("TEXT54", "Comentarios no encontrados.");
+
+?>
